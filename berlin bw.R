582d21387dacc95cac45ade5d96da09dd7448350
Daniela de los Santos <43679304+danidlsa@users.noreply.github.com>
2019-11-10 13:59:52 -0300

    Add files via upload

---
+++ berlin bw.R
@@ -0,0 +1,20 @@
+setwd("C:/Users/Usuario/Dropbox/datos de miércoles/30 map challenge")
+
+library(leaflet)
+library(rgdal)
+#install.packages("mapview")
+library(htmlwidgets)
+library(webshot)
+
+wall <- "https://opendata.arcgis.com/datasets/ef3c99e6dfcf4f84a90ae91820b08d7a_2.geojson"
+res <- readOGR(dsn = wall)
+
+m <- leaflet() %>% 
+  addProviderTiles(provider =
+                    providers$OpenStreetMap.BlackAndWhite) %>%
+  setView(lat=52.52, lng=13.4, zoom=10.5) %>% 
+  addPolylines(data = res, color="black")
+
+saveWidget(m, "temp.html", selfcontained = FALSE)
+webshot("temp.html", file = "berlin wall.png",
+        cliprect = "viewport")
